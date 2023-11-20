a1f9a39f7ae0b1f113c64c490594a346969dfec2
ToonvanBerkel <143796002+ToonvanBerkel@users.noreply.github.com>
2023-11-20 14:00:56 +0100

    dee het verkeerd

--- opdracht 2.4 functies met parameters/JS/app.js
+++ opdracht 2.4 functies met parameters/JS/app.js
@@ -1,9 +1,31 @@
-if(opr == "+") {
-    console.log(num1 + num2)
-} else if(opr == "-") {
-    console.log(num1 - num2)
-} else if(opr == "*") {
-    console.log(num1 * num2)
-} else if(opr == "/") {
-    console.log(num / num2)
+function calculate(num1, num2) {
+    let result = (num1 + num2)
+    console.log(result)
 }
+
+calculate(10, 320)
+
+function multiplyByFive(num1) {
+    let result = (num1 * 5)
+    console.log(result)
+}
+
+multiplyByFive(10)
+
+function calculateMinutes(num1) {
+    let result = (num1 / 60)
+    console.log(result)
+}
+
+calculateMinutes (10000)
+
+//vorige//
+//if(opr == "+") {//
+//    console.log(num1 + num2)//
+//} else if(opr == "-") {//
+//    console.log(num1 - num2)//
+//} else if(opr == "*") {//
+//    console.log(num1 * num2)//
+//} else if(opr == "/") {//
+//    console.log(num / num2)//
+//}//
