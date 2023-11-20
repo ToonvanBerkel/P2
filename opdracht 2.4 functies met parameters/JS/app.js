66af19293343320b9b5e3819672c17bfee5c9f00
ToonvanBerkel <143796002+ToonvanBerkel@users.noreply.github.com>
2023-11-20 13:44:55 +0100

    update

--- opdracht 2.4 functies met parameters/JS/app.js
+++ opdracht 2.4 functies met parameters/JS/app.js
@@ -1,7 +1,3 @@
-var opr = prompt("+,-,* of /: ")
-var num1 = parseInt(prompt("Nummer 1: "))
-var num2 = parseInt(prompt("Nummer 2: "))
-
 if(opr == "+") {
     console.log(num1 + num2)
 } else if(opr == "-") {
