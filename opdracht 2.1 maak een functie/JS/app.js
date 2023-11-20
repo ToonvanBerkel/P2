197dd3996125c85168b926355083c68ab1ac056d
ToonvanBerkel <143796002+ToonvanBerkel@users.noreply.github.com>
2023-11-20 13:06:40 +0100

    full

---
+++ opdracht 2.1 maak een functie/JS/app.js
@@ -0,0 +1,7 @@
+function kleur_aanpassen() {
+    document.getElementById('h1').style.color = "red"
+    console.log('de functie werkt');
+    document.getElementById('h1').innerHTML = "Inhoud aangepast!";
+}
+
+kleur_aanpassen();
