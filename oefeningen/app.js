36690ba883c2a2c5157aa25bcde6adf610994395
ToonvanBerkel <143796002+ToonvanBerkel@users.noreply.github.com>
2023-11-28 10:28:25 +0100

    oefeningen

---
+++ oefeningen/app.js
@@ -0,0 +1,29 @@
+const myName = "Toon van Berkel";
+console.log(myName);
+
+let myAge = "16";
+console.log(myAge);
+
+const diploma = true;
+console.log(diploma);
+
+let passed;
+console.log(passed);
+
+//calculator
+let num1 = 69;
+let num2 = 360;
+let answer = num1 + num2;
+console.log(answer);
+
+let answer2 = num1 - num2;
+console.log(answer2);
+
+let answer3 = num1 / num2;
+console.log(answer3);
+
+let answer4 = num1 * num2;
+console.log(answer4);
+
+let answer5 = num1 % num2;
+console.log(answer5);
