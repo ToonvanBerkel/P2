43129b3cbce6b8c7a6637efd8d6168b46bfc44b8
ToonvanBerkel <143796002+ToonvanBerkel@users.noreply.github.com>
2023-12-01 10:47:34 +0100

    fix

--- opdracht 3.1 - if else statement/app.js
+++ opdracht 3.1 - if else statement/app.js
@@ -2,7 +2,7 @@ let budget = 60;
 let product = 60;
 let element = document.getElementById("title");
 
-if (budget >= 60) {
+if (budget >= product) {
     element.innerText = "U heeft genoeg geld";
     element.style.color = "green";
 }else {
