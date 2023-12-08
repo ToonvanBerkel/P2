985d52a7d016bae4866cca93897bd8b403c8970c
ToonvanBerkel <143796002+ToonvanBerkel@users.noreply.github.com>
2023-12-08 12:57:52 +0100

    full

--- opdracht 4.1 calculator/app.js
+++ opdracht 4.1 calculator/app.js
@@ -1,22 +1,24 @@
 function add() {
     let num1 = parseFloat(document.getElementById("number_one").value);
     let num2 = parseFloat(document.getElementById("number_two").value);
-    document.getElementById("result").innerText = "Antwoord: " + (num1 * num2);
+    if (num2 !== 0) {
+        document.getElementById("result").innerText = "Antwoord: " + (num1 + num2);
+    } else {
+        document.getElementById("result").innerText = "Kan niet delen door 0";
+    }
 }
 
 function minus() {
     let num1 = parseFloat(document.getElementById("number_one").value);
     let num2 = parseFloat(document.getElementById("number_two").value);
-    document.getElementById("result").innerText = "Antwoord: " + (num1 * num2);
+    if (num2 !== 0) {
+        document.getElementById("result").innerText = "Antwoord: " + (num1 - num2);
+    } else {
+        document.getElementById("result").innerText = "Kan niet delen door 0";
+    }
 }
 
 function multiply() {
-    let num1 = parseFloat(document.getElementById("number_one").value);
-    let num2 = parseFloat(document.getElementById("number_two").value);
-    document.getElementById("result").innerText = "Antwoord: " + (num1 * num2);
-}
-
-function divide() {
     let num1 = parseFloat(document.getElementById("number_one").value);
     let num2 = parseFloat(document.getElementById("number_two").value);
     if (num2 !== 0) {
@@ -26,4 +28,12 @@ function divide() {
     }
 }
 
-console.log(num1)
+function divide() {
+    let num1 = parseFloat(document.getElementById("number_one").value);
+    let num2 = parseFloat(document.getElementById("number_two").value);
+    if (num2 !== 0) {
+        document.getElementById("result").innerText = "Antwoord: " + (num1 / num2);
+    } else {
+        document.getElementById("result").innerText = "Kan niet delen door 0";
+    }
+}
